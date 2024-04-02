19b6d7e4eaedb3188ec0b8e59631c3a559cbbdc9
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-04-02 17:47:25 +0530

    ok

---
+++ README.md
@@ -0,0 +1,11 @@
+<div align="center">
+    <img width=500 src="./examples/screenshot.png"/>
+
+# A syntax highlighter for buzz programming language in lite-xl!
+
+
+</div>
+
+
+
+[s](https://developer.apple.com/swift/)
---
+++ tests/test.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+// A syntax highlighter for Swift programming language in lite-xl!
+
+func hello() {
+    let name :String = "Rohan Vashisht";
+    let age :Int8 = 18;
+    print("Hi! I am \(name) and my age is \(age).");
+}
+
+hello()
